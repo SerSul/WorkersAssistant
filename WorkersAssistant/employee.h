5503e99e98d0ea4ee33240eb6d3ee9e4da38ff7b
SerSul <124405685+SerSul@users.noreply.github.com>
2023-07-27 20:21:56 +0300

    Update employee.h

--- WorkersAssistant/employee.h
+++ WorkersAssistant/employee.h
@@ -1,36 +1,29 @@
-#pragma once
-#include <stdlib.h>
-#include <iostream>
-#include <vector>
-#include <string>
-#include <cstdlib>
-#include "mysql_connection.h"
-#include <cppconn/driver.h>
-#include <cppconn/exception.h>
-#include <cppconn/prepared_statement.h>
-using namespace std;
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
 
-class employee
-{
+#include <string>
+
+class employee {
 private:
     int id;
-    string post;
-    string firstName;
-    string lastName;
+    std::string post;
+    std::string firstName;
+    std::string lastName;
     float monthlyPay;
-    string hireDate;
+    std::string hireDate;
     bool inStaff;
-public:
-    employee(int _id, const string& _post, const string& _firstName, const string& _lastName, float _monthlyPay, const string& _hireDate, bool _inStaff)
-        : id(_id), post(_post), firstName(_firstName), lastName(_lastName), monthlyPay(_monthlyPay), hireDate(_hireDate), inStaff(_inStaff)
-    {}
-    int getId() const { return id; }
-    string getFirstName() const { return firstName; }
-    string getPost() const { return post; }
-    string getLastName() const { return lastName; }
-    float getMonthlyPay() const { return monthlyPay; }
-    string getHireDate() const { return hireDate; }
-    bool isInStaff() const { return inStaff; }
 
+public:
+    employee(int _id, const std::string& _post, const std::string& _firstName, const std::string& _lastName, float _monthlyPay, const std::string& _hireDate, bool _inStaff)
+        : id(_id), post(_post), firstName(_firstName), lastName(_lastName), monthlyPay(_monthlyPay), hireDate(_hireDate), inStaff(_inStaff) {}
+
+    int getId() const { return id; }
+    std::string getFirstName() const { return firstName; }
+    std::string getPost() const { return post; }
+    std::string getLastName() const { return lastName; }
+    float getMonthlyPay() const { return monthlyPay; }
+    std::string getHireDate() const { return hireDate; }
+    bool isInStaff() const { return inStaff; }
 };
 
+#endif // EMPLOYEE_H
