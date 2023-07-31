29a86aecf02791815c0be837778021a1d53268a4
SerSul <124405685+SerSul@users.noreply.github.com>
2023-07-31 23:53:58 +0300

    Update main.cpp

--- WorkersAssistant/main.cpp
+++ WorkersAssistant/main.cpp
@@ -2,15 +2,23 @@
 using namespace std;
 
 
-const string server = "tcp://127.0.0.1:3306";
-const string username = "root";
-const string password = "123q";
+//const string server = "tcp://127.0.0.1:3306";
+//const string username = "root";
+//const string password = "";
 
+
+string server = "";
+string username = "";
+string password = "";
 
 int main()
 {
-
-
+    std::cout << "Введите хост";
+    std::cin >> server;
+    std::cout << "Введите пользователя";
+    std::cin >> username;
+    std::cout << "Введите пароль";
+    std::cin >> password;
     setlocale(LC_ALL, "Russian");
     sql::Driver* driver;
     sql::Connection* con;
